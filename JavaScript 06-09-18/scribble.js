f5a221c8fe595e5759df8ddee451e9ce25c4feb2
Garrett <garrett@users-MBP.home>
2018-05-31 21:52:44 -0700

    1

--- JavaScript 06-09-18/scribble.js
+++ JavaScript 06-09-18/scribble.js
@@ -6,11 +6,3 @@ var clip = scribble.clip({
 scribble.midi(clip);
 '''
 */
-const scribble = require('scribbletune');
-let chords = scribble.clip({
-	notes: 'F#m C#m DM Bm EM AM DM C#m AM',
-	pattern: 'x_x_x_--'.repeat(8),
-	sizzle: true
-});
-
-scribble.midi(chords, 'chords.mid');
---
+++ JavaScript Basics 06-02-18/Variables.js
@@ -0,0 +1,18 @@
+var bucky = 32;
+document.write(bucky);
+
+// JavaScript Data Types
+var tuna = 20; // Number
+var tuna = "text in here"; // String
+var tuna = true; // or false are known as: Booleans
+document.write(tuna);
+
+// First Exaple
+var name = "Bucky";
+var age  = 24;
+document.write(name + " is my name and my age is " + age);
+// Second Example
+var tuna  = "i am a fish";
+var bacon = "baconator?"
+var ham   = "hamburglarr"
+document.write(tuna+bacon+ham); 
