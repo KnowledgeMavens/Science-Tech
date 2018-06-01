0e77081725965fc13be763bcbfba11cbdb656486
Garrett <garrett@users-MBP.home>
2018-05-31 23:24:26 -0700

    1

--- JavaScript Basics 06-02-18/Functions.js
+++
@@ -1,8 +0,0 @@
-const scribble = require('scribbletune');
-let chords = scribble.clip({
-	notes: 'F#m C#m DM Bm EM AM DM C#m AM',
-	pattern: 'x_x_x_--'.repeat(8),
-	sizzle: true
-});
-
-scribble.midi(chords, 'chords.mid');
