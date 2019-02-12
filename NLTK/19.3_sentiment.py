bb7a96bec9469d6473ffd59280e19e8f4cf7720d
Garrett <garrett@macbook.local>
2019-02-11 23:41:02 -0800

    1

---
+++ NLTK/19.3_sentiment.py
@@ -0,0 +1,5 @@
+import sentiment_mod as s
+
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...yeah!"))
+
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible move, 0/10"))
