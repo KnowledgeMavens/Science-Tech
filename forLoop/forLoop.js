05d5fabcc240c7fedeb4bc3bbc337c36db711c07
Garrett <garrett@macbook.local>
2019-12-13 23:01:53 -0800

    1

---
+++ forLoop/forLoop.js
@@ -0,0 +1,12 @@
+//There are many types of for loops for, while, do while, for in, for of
+// We are going to be demonstrating the for loop in JavaScript
+
+//initial express or where loop should start i.e. (var i = 0;)
+//condition when loop should end i.e. (i < 5;)
+//increment expression loop goes up or down i.e. (x++)  || (x--)
+
+var languages = [JavaScript, Python, GoLang];
+
+for (var i = 0; i < 3; i++){
+    console.log(languages[i]);
+}
