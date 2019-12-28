134353fdac8e431511d9b25360eb41fb13304aa0
Garrett <garrett@macbook.local>
2019-12-28 09:00:44 -0600

    1

---
+++ JavaScript Arrays/array.js
@@ -0,0 +1,34 @@
+// What is an array = data structure of numbers, “strings of text”, objects
+
+var numbers = [ 50,, 9, 77, 101]
+var artists = [ "Eilish", "Cyrus", "Gaga", "Bieber"]
+//var songs = [{artist: "Eilish", song: "Bad Guy"}, {artist: "Gaga", song: "Telephone"}, {artist: "Cyrus", song:"Wreaking Ball"}]
+
+//See contents of array
+//console.log(artists)
+//console.log(artists.length)
+
+//add strings to end of array
+//artists.push("Bieber")
+//console.log(artists)
+
+//remove strings from end of array
+//artists.pop()
+
+console.log(artists.slice(1,3));
+
+
+//Add strings to beginning of array
+//artists.unshift("Bieber")
+//console.log(artists)
+
+
+//remove strings from beginning of array
+//artists.shift()
+//console.log(artists)
+
+//sort array
+//artists.reverse(artists)
+
+//artists.sort()
+//console.log(artists)
---
+++ JavaScript Arrays/index.js
@@ -0,0 +1,21 @@
+
+for(let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
+
+
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>JS Array Methods</title>
+</head>
+<body>
+    <h1> JS Array Methods</h1>
+
+    <script src="main.js"></script>
+</body>
+</html>
+*/
