0f384a72ddb7525fa85376be9a76bf5c8088b2b1
Garrett <garrett@macbook.local>
2019-12-30 08:33:27 -0600

    Create Functions.js

---
+++ JavaScript Functions/Functions.js
@@ -0,0 +1,17 @@
+// If variables are like nouns, functions are similiar to verbs
+
+/*
+function functionExample() {
+    console.log("Hello, my name is George");
+    console.log("example text 2");
+}
+functionExample();
+*/
+
+// functions with parameters and arguments
+
+function functionExample2(name) {  //parameter
+    console.log('Hello ' + name);
+}
+
+functionExample2(George); //argument
