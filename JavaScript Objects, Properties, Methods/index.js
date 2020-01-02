29a070f944116b985aa20f91b493fadf1a70e9e3
Garrett <garrett@macbook.local>
2020-01-02 08:35:28 -0600

    Update index.js

--- JavaScript Objects, Properties, Methods/index.js
+++ JavaScript Objects, Properties, Methods/index.js
@@ -1,1 +1,11 @@
-console.log("Why");
+let character = {
+    "name": "Mando",
+    "type": "Antihero",
+    "film": "Disney+",
+    "rating": [4, 3, 2]
+}
+
+console.log(character["name"])
+
+//How to display mutiple properties ???
+console.log(character, ['name', 'type']);
