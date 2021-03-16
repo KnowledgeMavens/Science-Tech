62a602ffc7745af688d5b8ff9782a9566b5707df
Garrett <garrett@MacBook-Pro.local>
2021-03-15 21:25:53 -0700

    1

---
+++ hackerrank/captains room.py
@@ -0,0 +1,11 @@
+group = int(input())
+rooms = list(map(int, input().split()))
+a = set()
+b = set()
+for i in rooms:
+    if i not in a:
+        a.add(i)
+        b.add(i)
+    else:
+        b.discard(i)
+print(b.pop())
